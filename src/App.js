15a6f0666b9f1735b43a418c24f6369119c29f01
ulyavrubel <uvrubel@gmail.com>
2020-03-11 21:46:39 +0800

    tailwindcss

--- src/App.js
+++ src/App.js
@@ -68,7 +68,8 @@ export default () => {
         draft.push([nick, message]);
       });
     });
-  }, 0);
+    // eslint-disable-next-line
+  }, []);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -106,8 +107,71 @@ export default () => {
       </div>
     </section>
   ) : (
-    <div style={{ textAlign: "center", margin: "30vh auto", width: "70%" }}>
-      <form onSubmit={event => handleSubmit(event)}>
+    <div class="container mx-auto">
+      <div
+        className="flex flex-row justify-center box-border p-4 border border-gray-200 w-1/3  inline-block shadow-lg"
+        style={{ textAlign: "center", margin: "30vh auto" }}
+      >
+        <div className="box-content ">
+          <form
+            onSubmit={event => handleSubmit(event)}
+            className="w-full max-w-sm"
+          >
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="name"
+                >
+                  Name
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  id="name"
+                  onChange={event => setNameInput(event.target.value.trim())}
+                  required
+                  placeholder="What is your name .."
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  type="text"
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="room"
+                >
+                  Room
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  id="room"
+                  onChange={event => setRoom(event.target.value.trim())}
+                  required
+                  placeholder="What is your room .."
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                />
+              </div>
+            </div>
+
+            <div className="md:flex md:items-center">
+              <div className="md:w-1/3"></div>
+              <div className="md:w-2/3">
+                <button
+                  className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      {/* <form onSubmit={event => handleSubmit(event)}>
         <input
           id="name"
           onChange={event => setNameInput(event.target.value.trim())}
@@ -123,7 +187,7 @@ export default () => {
         ></input>
         <br />
         <button type="submit">Submit</button>
-      </form>
+      </form> */}
     </div>
   );
 };
