6b9175e8c905b1911e24e29d3e83a340cf047c51
ulyavrubel <uvrubel@gmail.com>
2020-03-11 19:30:05 +0800

    Updates

---
+++ precache-manifest.af93c2e999f9c7b845843959c5365144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62cf16ecec4cb800de0d73db08d7eaa",
+    "url": "/chatApp/index.html"
+  },
+  {
+    "revision": "7d4ac52140bafcebc297",
+    "url": "/chatApp/static/css/main.d13c46bd.chunk.css"
+  },
+  {
+    "revision": "a6886be47e59b1a503fb",
+    "url": "/chatApp/static/js/2.65273125.chunk.js"
+  },
+  {
+    "revision": "1201c8656ff5734f581290e1df7d9b22",
+    "url": "/chatApp/static/js/2.65273125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4ac52140bafcebc297",
+    "url": "/chatApp/static/js/main.3c331dca.chunk.js"
+  },
+  {
+    "revision": "e383af41084701fa2469",
+    "url": "/chatApp/static/js/runtime-main.f656d95d.js"
+  }
+]);
