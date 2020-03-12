d70d935f9edcd7f853631a960c683bc5fb994a21
ulyavrubel <uvrubel@gmail.com>
2020-03-12 21:35:02 +0800

    styled w/ tailwind

--- src/App.js
+++ src/App.js
@@ -2,21 +2,39 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import useSocket from "use-socket.io-client";
 import { useImmer } from "use-immer";
+import { animateScroll } from "react-scroll";
 
-const Messages = props =>
-  props.data.map(m =>
+const Messages = props => {
+  let id = props.id;
+  console.log(id);
+
+  return props.data.map(m =>
     m[0] !== "" ? (
       <li key={m[0]}>
-        <strong>{m[0]}</strong> : <div className="innermsg">{m[1]}</div>
+        {id === m[0] ? (
+          <div className="rounded-full py-2 px-4 bg-blue-200 w-auto inline-block text-xs mt-1 ">
+            <strong>{m[0]}</strong> : {m[1]}
+          </div>
+        ) : (
+          <div className="rounded-full py-2 px-4 bg-gray-200 w-auto inline-block text-xs mt-1">
+            <strong>{m[0]}</strong> : {m[1]}
+          </div>
+        )}
       </li>
     ) : (
-      <li key={m[1]} className="update">
+      <li key={m[1]} className="update py-4">
         {m[1]}
       </li>
     )
   );
+};
 
-const Online = props => props.data.map(m => <li id={m[0]}>{m[1]}</li>);
+const Online = props =>
+  props.data.map(m => (
+    <li className="text-sm" id={m[0]}>
+      {m[1]}
+    </li>
+  ));
 
 export default () => {
   const [id, setId] = useState("");
@@ -85,94 +103,56 @@ export default () => {
     if (input !== "") {
       socket.emit("chat message", input, room);
       setInput("");
+      e.target.reset();
     }
   };
 
-  return id ? (
-    <section style={{ disply: "flex", flexDirection: "row" }}>
-      <ul id="messages">
-        <Messages data={messages} />
-      </ul>
-      <ul id="online">
-        {" "}
-        &#x1f310; : <Online data={online} />{" "}
-      </ul>
-      <div id="sendform">
-        <form onSubmit={e => handleSend(e)} style={{ display: "flex" }}>
-          <input id="m" onChange={e => setInput(e.target.value.trim())}></input>
-          <button style={{ width: "75px" }} type="submit">
-            Send
-          </button>
-        </form>
-      </div>
-    </section>
-  ) : (
-    <div class="container mx-auto">
-      <div
-        className="flex flex-row justify-center box-border p-4 border border-gray-200 w-1/3  inline-block shadow-lg"
-        style={{ textAlign: "center", margin: "30vh auto" }}
-      >
-        <div className="box-content ">
-          <form
-            onSubmit={event => handleSubmit(event)}
-            className="w-full max-w-sm"
-          >
-            <div className="md:flex md:items-center mb-6">
-              <div className="md:w-1/3">
-                <label
-                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                  for="name"
-                >
-                  Name
-                </label>
-              </div>
-              <div className="md:w-2/3">
-                <input
-                  id="name"
-                  onChange={event => setNameInput(event.target.value.trim())}
-                  required
-                  placeholder="What is your name .."
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                  type="text"
-                />
-              </div>
-            </div>
-            <div className="md:flex md:items-center mb-6">
-              <div className="md:w-1/3">
-                <label
-                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                  for="room"
-                >
-                  Room
-                </label>
-              </div>
-              <div className="md:w-2/3">
-                <input
-                  id="room"
-                  onChange={event => setRoom(event.target.value.trim())}
-                  required
-                  placeholder="What is your room .."
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                />
-              </div>
-            </div>
+  const scrollToBottom = () => {
+    animateScroll.scrollToBottom({
+      containerId: "messages"
+    });
+  };
 
-            <div className="md:flex md:items-center">
-              <div className="md:w-1/3"></div>
-              <div className="md:w-2/3">
-                <button
-                  className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  return id ? (
+    <div>
+      <section className="flex-row p-2">
+        <ul id="online" className="overflow-y-auto p-2 bg-gray-300 rounded ">
+          {" "}
+          &#x1f310; : <Online data={online} />{" "}
+        </ul>
+        <ul id="messages" className="overflow-y-auto px-2 ">
+          <Messages data={messages} id={id} />
+        </ul>
+
+        <div id="sendform" className="px-2 py-2">
+          <form onSubmit={e => handleSend(e)} style={{ display: "flex" }}>
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-100"
+              id="m"
+              onChange={e => setInput(e.target.value.trim())}
+            ></input>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-2"
+              type="submit"
+            >
+              Send
+            </button>
           </form>
         </div>
-      </div>
-      {/* <form onSubmit={event => handleSubmit(event)}>
+      </section>
+    </div>
+  ) : (
+    <div className="py-40 mx-auto w-4/5">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={event => handleSubmit(event)}
+      >
         <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
           id="name"
           onChange={event => setNameInput(event.target.value.trim())}
           required
@@ -180,14 +160,22 @@ export default () => {
         ></input>
         <br />
         <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-8"
           id="room"
           onChange={event => setRoom(event.target.value.trim())}
           required
           placeholder="What is your room .."
         ></input>
         <br />
-        <button type="submit">Submit</button>
-      </form> */}
+        <div className="flex justify-center ">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
